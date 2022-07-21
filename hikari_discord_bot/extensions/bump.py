0f21ed8bf92dd53804e403a641e94a661ae780e5
Najeeb Farhan <najeebfarhan6345@gmail.com>
2022-07-21 23:21:25 +0530

    created bump.py

---
+++ hikari_discord_bot/extensions/bump.py
@@ -0,0 +1,24 @@
+import lightbulb
+import hikari
+
+plugin = lightbulb.Plugin("TrackBumpPlugin")
+
+@plugin.listener(hikari.events.GuildMessageCreateEvent)
+async def track_bump(event: hikari.events.GuildMessageCreateEvent):
+    if str(event.channel_id) != "757071256096079953" and event.author_id != "302050872383242240":
+        return
+
+    bump_image = "https://disboard.org/images/bot-command-image-bump.png"
+
+    try:
+        # implement a database and scoreboard in futute
+        if event.embeds[0].image.url == bump_image:
+            pass
+    except AttributeError:
+        pass
+
+def load(bot: lightbulb.BotApp): 
+    bot.add_plugin(plugin)
+
+def unload(bot: lightbulb.BotApp):
+    bot.remove_plugin(plugin)
